2553ffb8916187b327147bd7a87d79c7f43d46d8
bdalwahedbdalhady123-eng <bdalwahedbdalhady123@gmail.com>
2026-05-29 07:58:36 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,43 +1,37 @@
-import re
+import socket
 import sys
+from datetime import datetime
 
-def check_password_strength(password):
-    # المعايير الأمنية
-    length_error = len(password) < 8
-    digit_error = re.search(r"\d", password) is None
-    uppercase_error = re.search(r"[A-Z]", password) is None
-    lowercase_error = re.search(r"[a-z]", password) is None
-    symbol_error = re.search(r"[ !@#$%^&*(),.?\":{}|<>_]", password) is None
-    
-    # حساب النتيجة
-    errors = [length_error, digit_error, uppercase_error, lowercase_error, symbol_error]
-    score = errors.count(False) # كلما قلّت الأخطاء زادت القوة
-    
-    print("-" * 40)
-    print(f"Password Evaluated: {password}")
-    print("-" * 40)
-    
-    # تحديد مستوى القوة
-    if score == 5:
-        return "🟢 Strength: VERY STRONG (Excellent!)"
-    elif score == 4:
-        return "🟡 Strength: STRONG (Good, but can be improved)"
-    elif score == 3:
-        return "🟠 Strength: MEDIUM (Vulnerable to attacks)"
-    else:
-        return "🔴 Strength: WEAK (Highly Insecure!)"
+target_host = input("Enter host to scan (e.g., 127.0.0.1): ")
 
-# تشغيل الأداة
 try:
-    while True:
-        user_password = input("\nEnter a password to test (or type 'exit' to quit): ")
-        if user_password.lower() == 'exit':
-            print("Exiting tool. Stay safe!")
-            sys.exit()
-            
-        result = check_password_strength(user_password)
-        print(result)
-        
+    target_ip = socket.gethostbyname(target_host)
+except socket.gaierror:
+    print("\n Hostname could not be resolved. Exiting.")
+    sys.exit()
+
+print("-" * 50)
+print(f"Scanning Target: {target_ip}")
+print(f"Time Started: {str(datetime.now())}")
+print("-" * 50)
+
+ports_to_scan = [21, 22, 23, 25, 80, 110, 443, 8080]
+
+try:
+    for port in ports_to_scan:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1.0)
+        result = s.connect_ex((target_ip, port))
+        if result == 0:
+            print(f"Port {port}: OPEN")
+        else:
+            print(f"Port {port}: Closed")
+        s.close()
+
 except KeyboardInterrupt:
-    print("\nExiting tool.")
+    print("\n Exiting script.")
+    sys.exit()
+
+except socket.error:
+    print("\n Could not connect to server.")
     sys.exit()
